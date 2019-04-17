3d323f2df2fd9d9c60d266c98ef1e1db0d27e4a7
hang <xiaohang.duan@quantgroup.cn>
2019-04-17 11:29:09 +0800

    获取非内网ip

--- src/main/java/cn/henry/springbootlearning/utils/HttpRequestHelper.java
+++ src/main/java/cn/henry/springbootlearning/utils/HttpRequestHelper.java
@@ -1,5 +1,8 @@
 package cn.henry.springbootlearning.utils;
 
+import org.apache.commons.lang3.StringUtils;
+import sun.net.util.IPAddressUtil;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -7,6 +10,7 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -25,6 +29,61 @@ public final class HttpRequestHelper {
         String header = request.getHeader("X-Requested-With");
         boolean isAjax = "XMLHttpRequest".equalsIgnoreCase(header);
         return isAjax;
+    }
+
+    /**
+     * 拿到第一个非内网ip
+     * @return
+     */
+    public static String storeIp(HttpServletRequest request) {
+        String ip = getIpAddr(request);
+        if (StringUtils.isNotEmpty(ip) && !"unKnown".equalsIgnoreCase(ip)) {
+            String[] ips = ip.split(", ");
+            if (ips.length > 1) {
+                for (String realIp : ips) {
+                    if (!internalIp(IPAddressUtil.textToNumericFormatV4(realIp))) {
+                        ip = realIp;
+                        break;
+                    }
+                }
+            }
+        }
+        return ip;
+    }
+
+    /**
+     * 是否内网ip:https://blog.csdn.net/teaey/article/details/42527771
+     * @param addr
+     * @return
+     */
+    private static boolean internalIp(byte[] addr) {
+        final byte b0 = addr[0];
+        final byte b1 = addr[1];
+        //10.x.x.x/8
+        final byte SECTION_1 = 0x0A;
+        //172.16.x.x/12
+        final byte SECTION_2 = (byte) 0xAC;
+        final byte SECTION_3 = (byte) 0x10;
+        final byte SECTION_4 = (byte) 0x1F;
+        //192.168.x.x/16
+        final byte SECTION_5 = (byte) 0xC0;
+        final byte SECTION_6 = (byte) 0xA8;
+        switch (b0) {
+            case SECTION_1:
+                return true;
+            case SECTION_2:
+                if (b1 >= SECTION_3 && b1 <= SECTION_4) {
+                    return true;
+                }
+            case SECTION_5:
+                switch (b1) {
+                    case SECTION_6:
+                        return true;
+                }
+            default:
+                return false;
+
+        }
     }
 
     /**
