c8fc824d58b623ec2bc765b2fbdf15563c50478f
hang <xiaohang.duan@quantgroup.cn>
2019-04-23 17:36:38 +0800

    优化切面日志

--- src/main/java/cn/henry/springbootlearning/aspect/log/LogAspect.java
+++ src/main/java/cn/henry/springbootlearning/aspect/log/LogAspect.java
@@ -1,17 +1,19 @@
 package cn.henry.springbootlearning.aspect.log;
 
-import cn.henry.springbootlearning.utils.HttpRequestHelper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.UUID;
 
 /**
  * @Description:记录日志  （参考：https://blog.csdn.net/zhengchao1991/article/details/53391244）
@@ -28,20 +30,44 @@ public class LogAspect {
 
     }
 
-    @Before("logAspect()")
-    public void doBefore(JoinPoint jp) {
-        //获得请求（https://www.cnblogs.com/xdp-gacl/p/3798347.html）
-        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        HttpServletRequest request = attributes.getRequest();
+    @Around("logAspect()")
+    public Object controllerLogAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        /**
+         * 获取 request 中包含的请求参数
+         */
+        String uuid = UUID.randomUUID().toString();
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        /**
+         * 获取切点请求参数(class,method)
+         */
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
 
-        //记录请求内容
-        log.info("URL : {} ;HTTP_METHOD : {} ;IP : {} ;ARGS : {}",
-                request.getRequestURL().toString(), request.getMethod(), HttpRequestHelper.getIpAddr(request), request.getQueryString());
+        StringBuilder params = new StringBuilder();
+        ObjectMapper mapper = new ObjectMapper();
+        if ("POST".equalsIgnoreCase(request.getMethod())) {
+            Object[] objects = joinPoint.getArgs();
+            for (Object arg : objects) {
+                params.append(mapper.writeValueAsString(arg));
+            }
+        }
+        if ("GET".equalsIgnoreCase(request.getMethod())) {
+            params.append(request.getQueryString());
+        }
+        /**
+         * 入参日志
+         */
+        log.info("[AOP-LOG-START]\n\trequestMark: {}\n\trequestIP: {}\n\tcontentType:{}\n\trequestUrl: {}\n\t" +
+                        "requestMethod: {}\n\trequestParams: {}\n\ttargetClassAndMethod: {}#{}", uuid, request.getRemoteAddr(),
+                request.getHeader("Content-Type"),request.getRequestURL(), request.getMethod(), params.toString(),
+                method.getDeclaringClass().getName(), method.getName());
+        /**
+         * 出参日志
+         */
+        Object result = joinPoint.proceed();
+        log.info("[AOP-LOG-END]\n\t{}", result);
+        return result;
     }
 
-    @AfterReturning(returning = "ret", pointcut = "logAspect()")
-    public void doAfterReturning(Object ret) {
-        log.info("response : " + ret);
-    }
 
 }
